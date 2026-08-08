a6ec0cff034c336dbd9bacc35e06bb843a767a5a
RobinHood P2P <stepweather@gmail.com>
2026-08-08 17:39:34 +0300

    Update ui.js

--- js/sherwood/ui.js
+++ js/sherwood/ui.js
@@ -542,8 +542,8 @@ _doStep: function(tx, ty) {
     if (res.type === 'potion') { this._stopSound('steps'); this._playSound('potion'); this._showInteractButton('potion'); return; }
     if (res.type === 'chest') { this._stopSound('steps'); this._playSound('chest_open'); this._showInteractButton('chest'); return; }
     if (res.type === 'lootBag') { this._stopSound('steps'); this._playSound('bag_drop'); this._showInteractButton('lootBag'); return; }
-    if (res.type === 'exit') { this._stopSound('steps'); this._stopMusic(); var reward = Sherwood.Dungeon.complete(); SherwoodUI.updateDisplay(); this._afterRewardAction = function() { SherwoodUI._playMusic('main_theme'); SherwoodUI.showDungeon(); }; this._showVictoryScreen({ exp: reward.exp, gold: reward.gold, silver: reward.silver }); }
-    if (res.type === 'exit_locked') { this._stopSound('steps'); this._showToast('Закрыто! Убейте всех монстров!'); }
+    if (res.type === 'exit') { this._stopSound('steps'); this._stopMusic(); var reward = Sherwood.Dungeon.complete(); SherwoodUI.updateDisplay(); this._afterRewardAction = function() { SherwoodUI._playMusic('main_theme'); SherwoodUI.showDungeon(); }; this._showVictoryScreen({ exp: reward.exp, gold: reward.gold, silver: reward.silver }); return; }
+    if (res.type === 'exit_locked') { this._stopSound('steps'); this._showToast('Закрыто! Убейте всех монстров!'); return; }
     if (res.type === 'move') { this._stopSound('steps'); }
 },
 
