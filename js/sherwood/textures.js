ab2657e27553d3d44460326d9f803aa1039f6e01
RobinHood P2P <stepweather@gmail.com>
2026-08-27 23:02:27 +0300

    Update textures.js

--- js/sherwood/textures.js
+++ js/sherwood/textures.js
@@ -18,31 +18,26 @@ const Textures = {
             return;
         }
         
-        // Стена
         this.loadImage('assets/assets2/Sherwood_Square/wall_area_1.png', (img) => {
             this.wall = img;
             this.checkLoaded(callback);
         });
         
-        // Пол
         this.loadImage('assets/assets2/Sherwood_Square/floor1.png', (img) => {
             this.floor = img;
             this.checkLoaded(callback);
         });
         
-        // Потолок
         this.loadImage('assets/assets2/Sherwood_Square/1area_ceiling_moon.png', (img) => {
             this.ceiling = img;
             this.checkLoaded(callback);
         });
         
-        // Дуб
         this.loadImage('assets/assets2/Sherwood_Square/oak_area.png', (img) => {
             this.oak = img;
             this.checkLoaded(callback);
         });
         
-        // Швы
         this.loadImage('assets/assets2/game_details/seam_bottom.png', (img) => {
             this.seamBottom = img;
             this.checkLoaded(callback);
@@ -53,18 +48,6 @@ const Textures = {
             this.checkLoaded(callback);
         });
         
-        // Слоты
-        this.loadImage('assets/assets2/game_details/bag_cell.png', (img) => {
-            this.buildings['bag_cell'] = img;
-            this.checkLoaded(callback);
-        });
-        
-        this.loadImage('assets/assets2/game_details/2wallet_cell.png', (img) => {
-            this.buildings['wallet_cell'] = img;
-            this.checkLoaded(callback);
-        });
-        
-        // Иконки
         const buildingIcons = {
             'Чат': 'chat_button.png',
             'Профиль': 'player_profile.png',
@@ -86,10 +69,9 @@ const Textures = {
             'Таланты': 'ranger_skills_button.png',
             'Ежедневные': 'daily_quests.png',
             'Кошель': 'wallet.png',
-            'Кузница_icon': 'forge.png',
         };
         
-        this.totalTextures = 1 + 1 + 1 + 1 + 2 + 2 + Object.keys(buildingIcons).length;
+        this.totalTextures = 1 + 1 + 1 + 1 + 2 + Object.keys(buildingIcons).length;
         
         for (const [name, file] of Object.entries(buildingIcons)) {
             this.loadImage(`assets/assets2/icons/${file}`, (img) => {
